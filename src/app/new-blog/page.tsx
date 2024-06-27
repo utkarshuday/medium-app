3493b6af0ecb15522ebd531f20f1dda36471a695
utkarshuday <120191592+utkarshuday@users.noreply.github.com>
2024-06-27 14:56:34 +0530

    add editor route

--- src/app/new-blog/page.tsx
+++ src/app/new-blog/page.tsx
@@ -1,11 +1,19 @@
-import { BlogLayout } from '@/ui/components/blog-layout';
+import TipTapEditor from '@/ui/components/editor';
+import EditorLayout from '@/ui/components/editor/editor-layout';
 import NavBar from '@/ui/components/nav/nav-bar';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'New Blog',
+};
 
 export default function NewBlog() {
   return (
     <>
       <NavBar isPublish={true} />
-      <BlogLayout>Hello</BlogLayout>
+      <EditorLayout>
+        <TipTapEditor />
+      </EditorLayout>
     </>
   );
 }
